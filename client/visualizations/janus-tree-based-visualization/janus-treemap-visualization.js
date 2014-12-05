941f0a904aacec1625b8d2d59085f0840a0047a0
isaachibou <isaac.hibou@gmail.com>
2014-12-05 19:10:38 +0100

    Treemap Up and Running Still Zoom Problems

--- client/visualizations/janus-tree-based-visualization/janus-treemap-visualization.js
+++ client/visualizations/janus-tree-based-visualization/janus-treemap-visualization.js
@@ -1,26 +1,35 @@
 JanusTreeMapVisualization = function(id) {
+    
+    /* ID */
     this.id = id;
 
+    /* Data Struct */
     this.tree = new Tree();
-    this.grandparent;
-
+    this.lastMaxDepth = 0;
+     
+    /* Database */
     this.body_messages = [];
     this.header_messages = [];
     
-    this.x;
-    this.y;
+    /* Graphics*/
+    this.margin = {top: 20, right: 0, bottom: 0, left: 0};
+    this.width = 960;
+    this.height = 500 - this.margin.top - this.margin.bottom;
 
-    this.margin;
-    this.width = 1000;
-    this.height = 600;
+    this.x = d3.scale.linear()
+        .domain([0, this.width])
+        .range([0, this.width]);
 
-    this.transitioning;
-    this.formatNumber;
+    this.y = d3.scale.linear()
+        .domain([0, this.height])
+        .range([0, this.height]);
 
-    this.treemaplayout = {};
     this.svg = {};
+    this.treemaplayout = {};
 
-    this.lastMaxDepth = 0;
+    /* Stuff */
+    this.formatNumber = d3.format(",d");
+    this.transitioning;    
 };
 
 /**
@@ -28,7 +37,7 @@ JanusTreeMapVisualization = function(id) {
  *  interaction is the interaction to add
  */ 
 JanusTreeMapVisualization.prototype.addInteraction = function(interaction) {
-    var headers, msg;
+     var headers, msg;
  
     headers = JSON.parse(interaction.headers);
     this.header_messages.push(headers);
@@ -47,101 +56,226 @@ JanusTreeMapVisualization.prototype.addInteraction = function(interaction) {
                     var node = new TreeNode("Treemap","");
                     this.tree.addMapNode(node, null);
 
+                    /*
                     if(this.tree.getMaxDepth() == 0)
                     {  
                          // GrandParent
                          this.grandparent.append("text")
-                         .attr("x", 0)
-                         .attr("y", 0)
-                         .attr("dy", "1em")
-                         .attr("dx", "1em")
                          .text(msg.agentType.split(/[.]+/).pop());
                     }
+                    */
                 } 
 
                 var contextnode = new TreeNode(msg.source.spaceId.contextID, tcontext);
                 this.tree.addMapNode(contextnode, "Treemap");
 
-                /* Not for the moment 
+                /* Not for the moment */
                 var spaceNode = new TreeNode(msg.source.spaceId.id, tspace);
-                this.tree.addMapNode(spaceNode, msg.source.spaceId.contextID);*/
+                this.tree.addMapNode(spaceNode, msg.source.spaceId.contextID); 
             }       
          }  
     }
 };
 
+
 /** 
  * Update the d3 visualization
  */
 JanusTreeMapVisualization.prototype.update = function() {
-    if(this.tree.root != null) {        
-        
+    if(this.tree.root != null) {          
         var jtm = this;
-        var left_margin = 280;
-        var padding_y = 0.5;
-        var nodes = this.treemaplayout.nodes(this.tree.root);
 
-       // console.log(nodes);
-
+     
 
         //Test if the removal of only modified element is better than removing everything and redrawing the whole hierarchy
         d3.selectAll(this.id + " g.node").remove();
-        d3.selectAll(this.id + " path.link").remove();
+        d3.selectAll(this.id + " g.depth").remove();
+        //d3.selectAll(this.id + " g").remove();
 
-        var node = this.svg.selectAll("g.treemapnode")
-            .data(nodes)
-            .enter().append("g")
-            .attr("class", "treemapnode");
+        initialize(this.tree.root, this.width, this.height);
+        accumulate(this.tree.root);
+        this.layout(this.tree.root);
+        this.display(this.tree.root); 
 
-        node.append("rect")
-            .call(position);
-
-        
-            
+      
     }
 
-    function position() {
-      this.attr("x", function(d) { return d.x + "px"; })
-          .attr("y", function(d) { return d.y + "px"; })
-          .attr("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
-          .attr("height", function(d) { return Math.max(0, d.dy - 1) + "px"; });
+    function initialize(root, width, height) {
+        root.x = root.y = 0;
+        root.dx = width;
+        root.dy = height;
+        root.depth = 0;
     }
 
+    // Aggregate the values for internal nodes. This is normally done by the
+    // treemap layout, but not here because of our custom implementation.
+    // We also take a snapshot of the original children (_children) to avoid
+    // the children being overwritten when layout is computed.
+    function accumulate(d) {
+      return (d._children = d.children)
+          ? d.value = d.children.reduce(function(p, v) { return p + accumulate(v); }, 0)
+          : d.value;
+    }
+
+     
 };
+
+/** 
+ * Layout
+ */
+JanusTreeMapVisualization.prototype.layout = function(d) {
+    var self = this;
+    if (d._children) {
+      this.treemaplayout.nodes({_children: d._children});
+      d._children.forEach(function(c) {
+        c.x = d.x + c.x * d.dx;
+        c.y = d.y + c.y * d.dy;
+        c.dx *= d.dx;
+        c.dy *= d.dy;
+        c.parent = d;
+        self.layout(c);
+      });
+    }
+}
+
+
+/** 
+ * Display
+ */
+JanusTreeMapVisualization.prototype.display = function(d) {
+    var self = this;
+    
+
+    function text(text) {
+      text.attr("x", function(d) { return self.x(d.x) + 6; })
+        .attr("y", function(d) { return self.y(d.y) + 6; });
+    }
+
+    function rect(rect) {
+      rect.attr("x", function(d) { return self.x(d.x); })
+          .attr("y", function(d) { return self.y(d.y); })
+          .attr("width", function(d) { return self.x(d.x + d.dx) - self.x(d.x); })
+          .attr("height", function(d) { return self.y(d.y + d.dy) - self.y(d.y); });
+    }
+
+    function name(d) {
+      return d.parent
+          ? name(d.parent) + "." + d.name
+          : d.name;
+    }
+
+    this.grandparent
+        .datum(d.parent)
+        .on("click", this.transition)
+      .select("text")
+        .text(d.name);
+
+    var g1 = this.svg.insert("g", ".grandparent")
+        .datum(d)
+        .attr("class", "depth");
+
+    var g = g1.selectAll("g")
+        .data(d._children)
+      .enter().append("g")
+
+    g.filter(function(d) { return d._children; })
+        .classed("children", true)
+        .on("click", transition);
+
+    g.selectAll(".child")
+        .data(function(d) { return d._children || [d]; })
+      .enter().append("rect")
+        .attr("class", "child")
+        .call(rect);
+
+    g.append("rect")
+        .attr("class", "parent")
+        .call(rect)
+      .append("title")
+        .text(function(d) { return self.formatNumber(d.value); });
+
+     
+
+    g.append("text")
+        .attr("dy", ".75em")
+        .text(function(d) { return d.name; })
+        .call(text);
+
+    function transition(d) {
+      if (this.transitioning || !d) return;
+      this.transitioning = true;
+
+      var g2 = self.display(d),
+          t1 = g1.transition().duration(750),
+          t2 = g2.transition().duration(750);
+
+      // Update the domain only after entering new elements.
+      self.x.domain([d.x, d.x + d.dx]);
+      self.y.domain([d.y, d.y + d.dy]);
+
+      // Enable anti-aliasing during the transition.
+      self.svg.style("shape-rendering", null);
+
+      // Draw child nodes on top of parent nodes.
+      self.svg.selectAll(".depth").sort(function(a, b) { return a.depth - b.depth; });
+
+      // Fade-in entering text.
+      g2.selectAll("text").style("fill-opacity", 0);
+
+      // Transition to the new view.
+      t1.selectAll("text").call(text).style("fill-opacity", 0);
+      t2.selectAll("text").call(text).style("fill-opacity", 1);
+      t1.selectAll("rect").call(rect);
+      t2.selectAll("rect").call(rect);
+
+      // Remove the old node when the transition is finished.
+      t1.remove().each("end", function() {
+        self.svg.style("shape-rendering", "crispEdges");
+        self.transitioning = false;
+      });
+    }
+
+    return g;
+  
+}
 
 /**
  * Constructs the svg and the layout of this visualization
  */
 JanusTreeMapVisualization.prototype.build = function() {
-   /* var svgWidthScale = 1.6;
-    var treeLayoutScale = 2.6; */
-    
-    /* Physical Appearance */
-    this.margin = {top: 20, right: 0, bottom: 0, left: 0};
-    this.width = 1000;
-    this.height = 500 - this.margin.top - this.margin.bottom,
 
-    /* Format */
-    this.formatNumber = d3.format(",d");
-
-    /* Actual Layout */
+    /* Layout */
     this.treemaplayout = d3.layout.treemap()
-        .size([this.height, this.width])
-        .value(function(d) { return d.count; });
+        .children(function(d, depth) { return depth ? null : d._children; })
+        .ratio(this.height / this.width * 0.5 * (1 + Math.sqrt(5)))
+        .round(false)
+        .value(function(d) { return d.count; })
+        .sort(function(a, b) { return a.value - b.value; });
 
-    /* Drawing */
+    /* SVG */
     this.svg = d3.select(this.id)
-        .attr("width", this.width)
-        .attr("height", this.height);
+        .attr("width", this.width + this.margin.left + this.margin.right)
+        .attr("height", this.height + this.margin.bottom + this.margin.top)
+        .style("margin-left", -this.margin.left + "px")
+        .style("margin.right", -this.margin.right + "px")
+      .append("g")
+        .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")")
+        .style("shape-rendering", "crispEdges");
 
-    /* Top Label */
+    /* Top Bar */
     this.grandparent = this.svg.append("g")
-        .attr("class", "grandparent");
+    .attr("class", "grandparent");
 
     this.grandparent.append("rect")
-        .attr("y", 0)
+        .attr("y", -this.margin.top)
         .attr("width", this.width)
-        .attr("height", 20);
+        .attr("height", this.margin.top);
+
+    this.grandparent.append("text")
+        .attr("x", 6)
+        .attr("y", 6 - this.margin.top)
+        .attr("dy", ".75em");
+
 };
 
 JanusTreeMapVisualization.prototype.displayMessage = function(id, visible) {
